6b70826b375df5e29b183bb40ac81a91ff0924b3
sinakman <snakmn@gmail.com>
2019-12-31 13:45:36 +0300

    AES-Encryption-I-Use
    AES algorithm for encrypting password

---
+++ AES Crypto Algorithm EX/PassCrypto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace DAL.ORM.Entity
+{
+    public static class PassCrypto
+    {
+
+        //Go to ProjectContext class in DAL.ORM.Context. 
+
+        public static string base64Encode(string sData)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[sData.Length];
+
+                encData_byte = System.Text.Encoding.UTF8.GetBytes(sData);
+
+                string encodedData = Convert.ToBase64String(encData_byte);
+
+                return encodedData;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
+        }
+
+        public static string base64Decode(string sData)
+        {
+            try
+            {
+                System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+                System.Text.Decoder utf8Decode = encoder.GetDecoder();
+                byte[] todecode_byte = Convert.FromBase64String(sData);
+                int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
+                char[] decoded_char = new char[charCount];
+                utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
+                string result = new String(decoded_char);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Decode" + ex.Message);
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ AES Crypto Algorithm EX/ProjectContext.cs
@@ -0,0 +1,75 @@
+﻿using DAL.ORM.Entity;
+using DAL.ORM.Map;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.ORM.Context
+{
+    //Go to SaveChanges() Method...
+
+    public class ProjectContext:DbContext
+    {
+        
+        //Some places have been cut to draw attention to the algorithm!!!
+
+        //is Here.
+        public override int SaveChanges()
+        {
+            var modifiedEntries = ChangeTracker.Entries().Where(x => x.State == EntityState.Modified || x.State == EntityState.Added).ToList();
+
+            string identity = WindowsIdentity.GetCurrent().Name;
+            string computerName = Environment.MachineName;
+            DateTime dateTime = DateTime.Now;
+
+            foreach (var item in modifiedEntries)
+            {
+                BaseEntity baseEntity = item.Entity as BaseEntity;
+
+                if (item!=null)
+                {
+                    if (item.State==EntityState.Added)
+                    {
+                        baseEntity.CreatedUserName = identity;
+                        baseEntity.CreatedCompName = computerName;
+                        baseEntity.CreateDate = dateTime;
+                    }
+
+                    else if (item.State==EntityState.Modified)
+                    {
+                        baseEntity.UpdatedUserName = identity;
+                        baseEntity.UpdateCompnName = computerName;
+                        baseEntity.UpdateDate = dateTime;
+                    }
+                }
+            }
+            foreach (var item in modifiedEntries)
+            {
+                Employee employee = item.Entity as Employee;
+                if (item!=null)
+                {
+                    if (item.State==EntityState.Added)
+                    {
+                        //Call Crypto Method when entity added.
+                        employee.Password = PassCrypto.base64Encode(employee.Password);
+                    }
+                    else if (item.State == EntityState.Modified)
+                    {
+                        //Call Crypto Method when entity modified.
+                        employee.Password = PassCrypto.base64Encode(employee.Password);
+                    }
+
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
+    }
+}
